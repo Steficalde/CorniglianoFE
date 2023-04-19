c6536f864afeed842f269491017163eb10df7f4c
SignoCreator <signosignodiego@gmail.com>
2023-04-19 12:59:49 +0200

    adjusting

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -1,5 +1,5 @@
 
-export default function Message({ message = "", issuer = true }) {
+export default function Message({children, issuer = true }) {
   const messageStyler = issuer
     ? {
         background: '#b3e899',
@@ -22,14 +22,14 @@ export default function Message({ message = "", issuer = true }) {
       }
   const direction = issuer ? 'flex-row-reverse' : null
   const float = issuer ? ' justify-end ' : ' justify-start '
-  return (
+    return (
     <div className={'  w-full flex' + float}>
       <div
         className={'flex w-fit max-w-[80%] gap-2  items-center m-2 px-4 py-2 ' + direction}
         style={{ ...messageStyler, position: 'relative' }}
       >
         <div className="absolute top-0 " style={{ borderBottom: '10px solid transparent', ...triangleStyle }} />
-        <p className="text-[16px]">{message}</p>
+          {children}
       </div>
     </div>
   )
--- src/components/chat/MessagesContainer.tsx
+++ src/components/chat/MessagesContainer.tsx
@@ -2,29 +2,42 @@ import React, { useEffect, useState } from 'react'
 import send from '../../assets/images/send.png'
 import Message from './Message'
 import {SERVER_URL} from "../../costants";
-
+import {ThreeDots} from "react-loader-spinner"
 export default function MessagesContainer() {
   const input = window.document.querySelector('#get_message')
-  //true se l'emittente è l'utente
   let issuer = true
   const [messages, setMessages] = useState([
     "ciao, sono un robot molto giovane e ancora tanto da imparare, ma credo di poterti essere d'aiuto",
   ])
-  async function chat() {
 
+  const [isLoading, setIsLoading] = useState(false)
+
+  async function chat() {
+    setIsLoading(true)
     // @ts-ignore
     const message = input.value
+    let data: string
     try {
       let form = new FormData()
       form.append('message', message)
-      const res: any = await fetch(`${SERVER_URL}/chat`, {method: 'POST', body: form, cache: 'no-store'})
-      const data = await res.text()
-      setMessages((messages) => [ ...messages,message,data ])
+      const res = await fetch(`${SERVER_URL}/chat`, {method: 'POST', body: form, cache: 'no-store'})
+      data = await res.text()
+
+    }catch(e){
+      // @ts-ignore
+      console.log(e.error)
+      setMessages((messages) => [ ...messages,"problema di connessione, riprovare" ])
+    } finally {
+
+      setMessages((messages) => [ ...messages,message ])
+      setTimeout(() =>{
+        setMessages((messages) => [ ...messages,data])
+        setIsLoading(false)
+      }, 1000)
+
       // @ts-ignore
       input.value = ''
-    }catch(e:any){
-      console.log(e.error)
-      setMessages((messages) => [ ...messages,message,"problema di connessione, riprovare" ])
+      return true
     }
   }
   useEffect(()=>{
@@ -32,55 +45,67 @@ export default function MessagesContainer() {
     // @ts-ignore
     messagesContainer.scrollTop = messagesContainer.scrollHeight;
   },[messages])
+  // @ts-ignore
   return (
-    <div
-      id="container-chat"
-      className="bg-gray-200  w-[350px] h-[500px] flex flex-col	justify-between rounded-xl shadow-lg overflow-hidden"
-    >
-      <div id="container-messages" className="overflow-auto flex flex-col mt-3 p-2">
-        {
+      <div
+          id="container-chat"
+          className="bg-gray-200 border-2 w-[350px] h-[500px] flex flex-col	justify-between rounded-xl overflow-hidden"
+          style={{
+            boxShadow:"0 0 15px 5px #00000030"
+          }}
+      >
+        <div id="container-messages" className="overflow-auto flex flex-col mt-3 p-2">
+          {
             messages.map((message, index) => {
               issuer = !issuer
-              return <Message message={message} key={index} issuer={issuer} ></Message>
+              return <Message key={index} issuer={issuer} >{message}</Message>
             })
-        }
-      </div>
-      <div id="container-sender" className=" h-[60px] w-full flex justify-center items-center">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault()
-            chat()
-          }}
-          className="flex gap-2  w-full h-[40px] items-center justify-center"
-        >
-          <textarea
-
-            id="get_message"
-            className=" rounded-full pt-2 w-full ml-2 resize-none outline-none h-full  bg-white text-black pl-4 "
-            onKeyDown={(e) => {
-
-              if (e.key === 'Enter' && !e.shiftKey) {
+          }
+          {isLoading && <Message issuer={false}>
+            <ThreeDots
+                height="30"
+                width="30"
+                radius="2"
+                color="#4fa94d"
+                ariaLabel="three-dots-loading"
+                wrapperStyle={{}}
+                visible={true}
+            />
+          </Message>}
+        </div>
+        <div id="container-sender" className=" h-[60px] w-full flex justify-center items-center mb-2">
+          <form
+              onSubmit={(e) => {
                 e.preventDefault()
-                // @ts-ignore
-                if (input.value.length === 0) return
-                chat()
-              } else if (e.key === ' ') {
-                e.preventDefault()
-                // @ts-ignore
-                input.value += ' '
-              }
-            }}
-            autoComplete="off"
-          />
-          <button
-            type="submit"
-            value="enter"
-            className="w-fit aspect-square rounded-full p-2 mr-2 bg-my-dark-blue"
+              }}
+              className="flex gap-2  w-full h-[40px] items-center justify-center"
           >
-            <img src={send} width={20} height={20} alt="enter" />
-          </button>
-        </form>
+          <textarea
+              id="get_message"
+              className="rounded-full pt-2 w-full ml-2 resize-none outline-none h-full  bg-white text-black pl-4"
+              onKeyDown={(e) => {
+                if (e.key === 'Enter' && !e.shiftKey) {
+                  e.preventDefault()
+                  // @ts-ignore
+                  if (input.value.length === 0) return
+                  chat()
+                } else if (e.key === ' ') {
+                  e.preventDefault()
+                  // @ts-ignore
+                  input.value += ' '
+                }
+              }}
+              autoComplete="off"
+          />
+            <button
+                type="submit"
+                value="enter"
+                className="w-fit aspect-square rounded-full p-2 mr-2 bg-my-dark-blue"
+            >
+              <img src={send} width={20} height={20} alt="enter" />
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
   )
 }
